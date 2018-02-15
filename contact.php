971b2530b3d5968dca731cf1038537c3b7d13ac8
sam3106 <32195526+sam3106@users.noreply.github.com>
2018-02-15 18:28:55 -0500

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="contact.css">
+  <title>Teron Sampson Portfolio</title>
+    
+</head>
+<body>
+  <nav >
+    <div class="container ">
+     <div class="row mb-5">
+         <div class="col-sm-2 font-weight-bold"><a href="index.php">Home</a></div>
+         <div class="col-sm-4 font-weight-bold"><a href="myExpertise.php">My Expertise</a></div>
+         <div class="col-sm-2 font-weight-bold"><a href="projects.php">Projects</a></div>
+         <div class="col-sm-4 font-weight-bold"><a href="contact.php">Contact Me</a></div>
+     </div>
+    </div>
+   </nav>
+
+ <div class="container  text-center">
+       <div class="row  text-center">
+         <div class="col-12-sm center text-center">
+          <h1 class="descend font-weight-bold">CONTACT ME</h1>
+       </div>
+    </div>
+    </div>
+    
+    <div class="container text-center">
+        <div class="row">
+          <div class="col-sm-4 mb-3 ">
+            <a href="mailto:sampson2448@yahoo.com"><img class="img-fluid" src="email.png" /></a>
+          </div>
+          <div class="col-sm-4 mb-3 "> 
+            <a href="https://www.linkedin.com/in/teron-sampson-76b22111a/"><img class="img-fluid" src="linkedin.png" /></a>
+          </div>
+          <div class="col-sm-4 mb-3">
+            <a href="resume.pdf" download><img class="img-fluid" src="resume.png" /></a>
+          </div>
+        </div>
+    </div>
+    
+</body>
+</html>
---
+++ projects (1).php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="projects.css">
+  <title>Teron Sampson Portfolio</title>
+    
+</head>
+<body>
+  <nav class="text-center">
+    <div class="container">
+     <div class="row mb-5">
+         <div class="col-sm-2 font-weight-bold"><a href="index.php">Home</a></div>
+         <div class="col-sm-4 font-weight-bold"><a href="myExpertise.php">My Expertise</a></div>
+         <div class="col-sm-2 font-weight-bold"><a href="projects.php">Projects</a></div>
+         <div class="col-sm-4 font-weight-bold"><a href="contact.php">Contact Me</a></div>
+     </div>
+    </div>
+   </nav>
+   
+   <div class="container  text-center mb-4">
+       <div class="row  text-center">
+         <div class="col-12-sm center text-center">
+          <h1 class="descend font-weight-bold">MY PROJECTS</h1>
+       </div>
+    </div>
+  </div>
+   <div class="container text-center ">
+     <div class="row" >
+        <div class="col-sm-6 mb-2">
+        <h3>Draw A Name</h3>
+        <p>Languages used:PHP,MYSQL,HTML,CSS</p>
+       <a href="../Draw-A-Name-PHP-Project/create.php"><img class="img-fluid" src="../Draw-A-Name-PHP-Project/draw.png"></a>
+        </div>
+    
+       <div class="col-sm-6 mb-2">
+        <h3>Animal in my house app?</h3>
+        <p>Languages used:HTML,CSS,JavaScript,jQuery</p>
+         <a href="/animal-project/animal.html"><img class="img-fluid" src="/animal-project/animal.JPG"></a>
+        </div>
+       </div>
+       
+       <div class="row">
+        <div class="col-sm-6  mb-2">
+         <h3>GameStore</h3>
+        <p>Languages used:HTML,CSS,JavaScript</p>
+         <a href="../Game-Store/gamestore.html"><img class="img-fluid" src="../Game-Store/game2.JPG"></a>
+        </div>
+     <div class="col-sm-6 mb-2">
+        <h3>Restaurants</h3>
+        <p>Languages used:HTML,CSS,JavaScript</p>
+        <a href="../restaurant-example/restaurant.html"><img class="img-fluid" src="../restaurant-example/restaurant.jpg"></a>
+        </div>        
+     </div>
+    </div>
+    
+    </body>
+</html>
