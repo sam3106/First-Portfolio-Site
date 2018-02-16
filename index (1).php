80d417c7baaaac0a177cf1a47b9a4a9ec9b858b4
sam3106 <32195526+sam3106@users.noreply.github.com>
2018-02-16 10:59:36 -0500

    Update index (1).php

--- index (1).php
+++ index (1).php
@@ -9,31 +9,30 @@
     
 </head>
 <body onload="typeWriter()">
-  <nav>
+  <nav class="text-center">
     <div class="container">
-     <div class="row mt-2">
-         <div class="col-sm-2 font-weight-bold"><a href="index.php">Home</a></div>
-         <div class="col-sm-4 font-weight-bold"><a href="myExpertise.php">My Expertise</a></div>
-         <div class="col-sm-2 font-weight-bold"><a href="projects.php">Projects</a></div>
-         <div class="col-sm-4 font-weight-bold"><a href="contact.php">Contact Me</a></div>
-
+     <div class="row fix-row mb-5">
+         <div class="col-sm-3 font-weight-bold"><a href="index.php">Home</a></div>
+         <div class="col-sm-3 font-weight-bold"><a href="myExpertise.php">My Expertise</a></div>
+         <div class="col-sm-3 font-weight-bold"><a href="projects.php">Projects</a></div>
+         <div class="col-sm-3 font-weight-bold"><a href="contact.php">Contact Me</a></div>
      </div>
     </div>
    </nav>
    
-   
-   
-      <div class="container  text-center">
-       <div class="row  text-center">
+   <div class="container  text-center mb-5">
+       <div class="row fix-row text-center">
          <div class="col-12-sm center text-center">
-          <h1 class="descend font-weight-bold">ABOUT ME</h1>
+          <h1 class="descend font-weight-bold">Welcome to my Website!</h1>
        </div>
     </div>
-
+    
+   
        <div class="row pad text-center">
-          
            <div class="col-md-4"><img class="img-fluid left" src="1.jpg" alt="A picture of me when I was in the navy"></div>
-             <div class="col-md-8 right-two text-justify"><p>I am a professional, proficient, and polite individual. For four years, I was a part of the United State Navy. This honorable experience allowed me to serve my country. In addition, I was able to obtain valuable skills and training. While working as a team member in the U.S. Navy, I was promoted to a leadership position. After being in the military, I have successful spoke at speaking engagements and received my Realtor License. Through a series of coding creative projects, I discovered a passion for coding. The ability to create something out of nothing is a skill I truly value. Currently, I am enrolled at Great Bay Community College. My major is Computer Science. I have a strong support system - my lovely wife, Brenda, and my adorable dog Lassie.</p></div>
+             <div class="col-md-8 right-two text-justify"><p>Hi! My Name is Teron and I have a passion for web development. The ability to create something out of nothing is a skill I truly value. I love it so much so that I decided to attend Great Bay Community College where I am pursuing a degree in Computer Science. I am also a veteran of The United State Navy.
+                During the time I served, I was able to learn valuable life lessons such as attention to detail, leadership, discipline, being a self starter, working with others on a team, and sacrifice. 
+                I believe these skills translate when it comes to web developement, and I believe I would be a good fit for your company or organization. I have a strong support system while I persue my dreams - my lovely wife, Brenda, and my adorable dog Lassie.</p></div>
    </div>
    </div>
    
