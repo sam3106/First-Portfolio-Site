1785d0917fb1e0cf0d0b0af51b95104268abe1b0
sam3106 <32195526+sam3106@users.noreply.github.com>
2018-02-16 04:19:41 -0500

    Update projects (1).php

--- projects (1).php
+++ projects (1).php
@@ -11,22 +11,23 @@
 <body>
   <nav class="text-center">
     <div class="container">
-     <div class="row mb-5">
-         <div class="col-sm-2 font-weight-bold"><a href="index.php">Home</a></div>
-         <div class="col-sm-4 font-weight-bold"><a href="myExpertise.php">My Expertise</a></div>
-         <div class="col-sm-2 font-weight-bold"><a href="projects.php">Projects</a></div>
-         <div class="col-sm-4 font-weight-bold"><a href="contact.php">Contact Me</a></div>
+     <div class="row fix-row mb-5">
+         <div class="col-sm-3 font-weight-bold"><a href="index.php">Home</a></div>
+         <div class="col-sm-3 font-weight-bold"><a href="myExpertise.php">My Expertise</a></div>
+         <div class="col-sm-3 font-weight-bold"><a href="projects.php">Projects</a></div>
+         <div class="col-sm-3 font-weight-bold"><a href="contact.php">Contact Me</a></div>
      </div>
     </div>
    </nav>
    
-   <div class="container  text-center mb-4">
-       <div class="row  text-center">
+   <div class="container  text-center mb-5">
+       <div class="row fix-row text-center">
          <div class="col-12-sm center text-center">
-          <h1 class="descend font-weight-bold">MY PROJECTS</h1>
+          <h1 class="descend font-weight-bold">My Project</h1>
        </div>
     </div>
-  </div>
+    </div>
+
    <div class="container text-center ">
      <div class="row" >
         <div class="col-sm-6 mb-2">
