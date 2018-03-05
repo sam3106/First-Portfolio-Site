da4786a7a17a85c43a7a225274476ce87dca80fc
Teron Sampson <32195526+sam3106@users.noreply.github.com>
2018-03-05 11:48:40 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,10 @@
     
 </head>
 <body onload="typeWriter()">
+ <div class="page cf">
   <nav class="text-center">
     <div class="container">
-     <div class="row fix-row mb-5">
+     <div class="row fix-row ">
          <div class="col-sm-3 font-weight-bold"><a href="index.php">Home</a></div>
          <div class="col-sm-3 font-weight-bold"><a href="myExpertise.php">My Expertise</a></div>
          <div class="col-sm-3 font-weight-bold"><a href="projects.php">Projects</a></div>
@@ -20,32 +21,29 @@
     </div>
    </nav>
    
-   <div class="container  text-center mb-5">
+   <div class="container  text-center">
        <div class="row fix-row text-center">
          <div class="col-12-sm center text-center">
-          <h1 class="descend font-weight-bold">Welcome to my Website!</h1>
+          <h1 class="descend font-weight-bold">Welcome to my Portfolio Site!</h1>
        </div>
     </div>
     
-   
+   <section>
+      <div class="container">
        <div class="row pad text-center">
-           <div class="col-md-4"><img class="img-fluid left" src="1.jpg" alt="A picture of me when I was in the navy"></div>
-             <div class="col-md-8 right-two text-justify"><p>Hi! My Name is Teron and I have a passion for web development. The ability to create something out of nothing is a skill I truly value. I love it so much so that I decided to attend Great Bay Community College where I am pursuing a degree in Computer Science. I am also a veteran of The United State Navy.
-                During the time I served, I was able to learn valuable life lessons such as attention to detail, leadership, discipline, being a self starter, working with others on a team, and sacrifice. 
-                I believe these skills translate when it comes to web developement, and I believe I would be a good fit for your company or organization. I have a strong support system while I persue my dreams - my lovely wife, Brenda, and my adorable dog Lassie.</p></div>
-   </div>
-   </div>
-   
+        <div class="col-md-4 img left"></div>
+           <div class="col-md-8 right-two text-justify"><h3 class="text-center font-weight-bold ">Greetings!</h3><p class="text-justify text-center"> My Name is Teron and I have a passion for web development. The ability to create something     out of nothing is a skill I truly value. I love it so much so that I decided to attend Great Bay Community College where I am pursuing a      degree in Computer Science. I am also a veteran of The United State Navy.During the time I served, I was able to learn valuable life lessons such  as attention to detail, leadership, discipline, being a self starter, working with others on a team, and sacrifice.I believe these skills     translate when it comes to web developement, and I believe I would be a good fit for your company or organization. I have a strong support system   while I persue my dreams - my lovely wife, Brenda, and my adorable dog Lassie.</p>
+         </div>
+         </div>
+      </div>
 
-    <footer>
-
-     <div class="row-fluid text-center">
+   </section>
+     <div class="row-fluid text-center bottom cf">
           <div id="type-writer">
           </div>
     </div>
-
-    </footer>
-
+    </div>
+     </div>
  <script src="index.js"></script>  
 </body>
 </html>
